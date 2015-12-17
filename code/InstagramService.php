fe0985ce11a9989889467ce83e7be3dd245c82af
helpfulrobot <chris@silverstripe.com>
2015-12-18 11:33:04 +1300

    Converted to PSR-2

--- code/InstagramService.php
+++ code/InstagramService.php
@@ -1,6 +1,7 @@
 <?php
 
-class InstagramService extends RestfulService {
+class InstagramService extends RestfulService
+{
     public $connection;
 
     private $clientID;
@@ -9,35 +10,35 @@ class InstagramService extends RestfulService {
 
     public $config;
 
-	protected $api_urls = array(
-    	'user'						=> 'v1/users/%s/?access_token=%s',
-        'user_feed'					=> 'v1/users/self/feed?access_token=%s&max_id=%s&min_id=%s',
-        'user_recent'				=> 'v1/users/%s/media/recent/?access_token=%s&max_id=%s&min_id=%s&max_timestamp=%s&min_timestamp=%s',
-        'user_search'				=> 'v1/users/search?q=%s&access_token=%s',
-        'user_follows'				=> 'v1/users/%s/follows?access_token=%s',
-        'user_followed_by'			=> 'v1/users/%s/followed-by?access_token=%s',
-        'user_requested_by'			=> 'v1/users/self/requested-by?access_token=%s',
-        'user_relationship'			=> 'v1/users/%s/relationship?access_token=%s',
-        'modify_user_relationship'	=> 'v1/users/%s/relationship?action=%s&access_token=%s',
-        'media'						=> 'v1/media/%s?access_token=%s',
-        'media_search'				=> 'v1/media/search?lat=%s&lng=%s&max_timestamp=%s&min_timestamp=%s&distance=%s&access_token=%s',
-        'media_popular'				=> 'v1/media/popular?access_token=%s',
-        'media_comments'			=> 'v1/media/%s/comments?access_token=%s',
-        'post_media_comment'		=> 'v1/media/%s/comments?access_token=%s',
-        'delete_media_comment'		=> 'v1/media/%s/comments?comment_id=%s&access_token=%s',
-        'likes'						=> 'v1/media/%s/likes?access_token=%s',
-    	'post_like'					=> 'v1/media/%s/likes?access_token=%s',
-        'remove_like'				=> 'v1/media/%s/likes?access_token=%s',
-        'tags'						=> 'v1/tags/%s?access_token=%s',
-        'tags_recent'				=> 'v1/tags/%s/media/recent?max_tag_id=%s&min_tag_id=%s&access_token=%s',
-        'tags_search'				=> 'v1/tags/search?q=%s&access_token=%s',
-        'locations'					=> 'v1/locations/%d?access_token=%s',
-        'locations_recent'			=> 'v1/locations/%d/media/recent/?max_id=%s&min_id=%s&max_timestamp=%s&min_timestamp=%s&access_token=%s',
-        'locations_search'			=> 'v1/locations/search?lat=%s&lng=%s&foursquare_id=%s&distance=%s&access_token=%s'
+    protected $api_urls = array(
+        'user'                        => 'v1/users/%s/?access_token=%s',
+        'user_feed'                    => 'v1/users/self/feed?access_token=%s&max_id=%s&min_id=%s',
+        'user_recent'                => 'v1/users/%s/media/recent/?access_token=%s&max_id=%s&min_id=%s&max_timestamp=%s&min_timestamp=%s',
+        'user_search'                => 'v1/users/search?q=%s&access_token=%s',
+        'user_follows'                => 'v1/users/%s/follows?access_token=%s',
+        'user_followed_by'            => 'v1/users/%s/followed-by?access_token=%s',
+        'user_requested_by'            => 'v1/users/self/requested-by?access_token=%s',
+        'user_relationship'            => 'v1/users/%s/relationship?access_token=%s',
+        'modify_user_relationship'    => 'v1/users/%s/relationship?action=%s&access_token=%s',
+        'media'                        => 'v1/media/%s?access_token=%s',
+        'media_search'                => 'v1/media/search?lat=%s&lng=%s&max_timestamp=%s&min_timestamp=%s&distance=%s&access_token=%s',
+        'media_popular'                => 'v1/media/popular?access_token=%s',
+        'media_comments'            => 'v1/media/%s/comments?access_token=%s',
+        'post_media_comment'        => 'v1/media/%s/comments?access_token=%s',
+        'delete_media_comment'        => 'v1/media/%s/comments?comment_id=%s&access_token=%s',
+        'likes'                        => 'v1/media/%s/likes?access_token=%s',
+        'post_like'                    => 'v1/media/%s/likes?access_token=%s',
+        'remove_like'                => 'v1/media/%s/likes?access_token=%s',
+        'tags'                        => 'v1/tags/%s?access_token=%s',
+        'tags_recent'                => 'v1/tags/%s/media/recent?max_tag_id=%s&min_tag_id=%s&access_token=%s',
+        'tags_search'                => 'v1/tags/search?q=%s&access_token=%s',
+        'locations'                    => 'v1/locations/%d?access_token=%s',
+        'locations_recent'            => 'v1/locations/%d/media/recent/?max_id=%s&min_id=%s&max_timestamp=%s&min_timestamp=%s&access_token=%s',
+        'locations_search'            => 'v1/locations/search?lat=%s&lng=%s&foursquare_id=%s&distance=%s&access_token=%s'
     );
 
-	public function __construct($sub=false, $header = null, $expiry=-1) {
-
+    public function __construct($sub=false, $header = null, $expiry=-1)
+    {
         parent::__construct('https://api.instagram.com/', $expiry);
 
         $this->clientID = Config::inst()->get('Instagram', 'clientID');
@@ -45,21 +46,23 @@ class InstagramService extends RestfulService {
 
         $this->config = SiteConfig::current_site_config();
 
-        if($sub) {
+        if ($sub) {
             $this->access_token = $sub->AccessToken;
         } else {
             $this->access_token = false;
         }
-	}
+    }
 
-    public static function loginLink($subID) {
+    public static function loginLink($subID)
+    {
         $clientID = Config::inst()->get('Instagram', 'clientID');
         $callback = Director::absoluteBaseURL().Config::inst()->get('Instagram', 'callbackUrl');
         Session::set('InstaSub', $subID);
         return 'https://api.instagram.com/oauth/authorize/?client_id='.$clientID.'&redirect_uri='.$callback.'&response_type=code';
     }
 
-    public function authorize($code) {
+    public function authorize($code)
+    {
         $callback = Director::absoluteBaseURL().Config::inst()->get('Instagram', 'callbackUrl');
 
         $auth_url = 'oauth/access_token';
@@ -75,7 +78,8 @@ class InstagramService extends RestfulService {
         Controller::curr()->redirect('admin/instagram-settings/InstagramSubscription/EditForm/field/InstagramSubscription/item/'.$subID.'/edit');
     }
 
-    public function subscribeRealtime() {
+    public function subscribeRealtime()
+    {
         $subID = $_GET['subscription'];
         $sub = InstagramSubscription::get()->byID($subID);
 
@@ -83,28 +87,28 @@ class InstagramService extends RestfulService {
 
         $type = $sub->Type;
         
-        if($type !== 'tag' && $type !== 'user') {
+        if ($type !== 'tag' && $type !== 'user') {
             throw new Exception('Subscription type is invalid');
         }
 
         $url = 'v1/subscriptions';
-        if($type=='tag') {
+        if ($type=='tag') {
             $key = $sub->Hashtag;
             $data = array(
                 "client_id"=> $this->clientID,
                 'client_secret' => $this->clientSecret,
                 'object' => 'tag',
                 'object_id' => $key,
-                'aspect' => 'media', 
+                'aspect' => 'media',
                 //'verify_token' => $this->access_token,
                 'callback_url' => $callback
             );
-        } else if($type == 'user') {
+        } elseif ($type == 'user') {
             $data = array(
                 "client_id"=> $this->clientID,
                 'client_secret' => $this->clientSecret,
                 'object' => 'user',
-                'aspect' => 'media', 
+                'aspect' => 'media',
                 'verify_token' => $sub->AccessToken,
                 'callback_url' => $callback
             );
@@ -112,20 +116,21 @@ class InstagramService extends RestfulService {
 
         $res = $this->json_request($url, 'POST', $data);
 
-        if($res->meta->code == 200) {
+        if ($res->meta->code == 200) {
             $sub->SubscriptionID = $res->data->id;
-            $sub->write();     
+            $sub->write();
             return true;
         } else {
-           throw new Exception('Error Subscribing to Instagram');
+            throw new Exception('Error Subscribing to Instagram');
         }
     }
 
-    public function unsubscribeRealtime() {
+    public function unsubscribeRealtime()
+    {
         $subID = $_GET['subscription'];
 
         $sub = InstagramSubscription::get()->byID($subID);
-        if(!$sub) {
+        if (!$sub) {
             throw new Exception('Unable to find Subscription.');
         }
 
@@ -137,7 +142,7 @@ class InstagramService extends RestfulService {
 
         $res = $this->json_request($url, "DELETE");
 
-        if($res->meta->code == 200) {
+        if ($res->meta->code == 200) {
             $sub->SubscriptionID = '';
             $sub->MinID = '';
             $sub->write();
@@ -147,14 +152,16 @@ class InstagramService extends RestfulService {
         }
     }
 
-    protected function json_request($url, $method = 'GET', $data=null) {
+    protected function json_request($url, $method = 'GET', $data=null)
+    {
         $res = $this->request($url, $method, $data);
 
         return json_decode($res->getBody());
     }
 
-    protected function access_token() {
-        if(!$this->access_token) {
+    protected function access_token()
+    {
+        if (!$this->access_token) {
             throw new Exception('Missing a subscription to use with Instagram.');
         }
         return $this->access_token;
@@ -166,7 +173,8 @@ class InstagramService extends RestfulService {
      * @param int Instagram user id
      * @return std_class data about the Instagram user
      */
-    public function getUser($user_id) {
+    public function getUser($user_id)
+    {
         $req_url = sprintf($this->api_urls['user'], $user_id, $this->access_token());
 
         return $this->json_request($req_url);
@@ -179,7 +187,8 @@ class InstagramService extends RestfulService {
      * @param int return media before min id
      * @return std_class of user's feed
      */
-    public function getUserFeed($max = null, $min = null) {
+    public function getUserFeed($max = null, $min = null)
+    {
         $url = sprintf($this->api_urls['user_feed'], $this->access_token(), $max, $min);
 
         return $this->json_request($url);
@@ -195,7 +204,8 @@ class InstagramService extends RestfulService {
      * @param int return media before this UNIX timestamp
      * @return std_class of media found based on parameters given
      */
-    public function getUserRecent($user_id, $max_id = null, $min_id = null, $max_timestamp = null, $min_timestamp = null) {
+    public function getUserRecent($user_id, $max_id = null, $min_id = null, $max_timestamp = null, $min_timestamp = null)
+    {
         $url = sprintf($this->api_urls['user_recent'], $user_id, $this->access_token(), $max_id, $min_id, $max_timestamp, $min_timestamp);
 
         return $this->json_request($url);
@@ -207,7 +217,8 @@ class InstagramService extends RestfulService {
      * @param string an Instagram user name
      * @return std_class user data
      */
-    public function userSearch($user_name) {
+    public function userSearch($user_name)
+    {
         $url = sprintf($this->api_urls['user_search'], $user_name, $this->access_token());
 
         return $this->json_request($url);
@@ -219,7 +230,8 @@ class InstagramService extends RestfulService {
      * @param int user id
      * @return std_class user's recent feed items
      */
-    public function userFollows($user_id) {
+    public function userFollows($user_id)
+    {
         $url = sprintf($this->api_urls['user_follows'], $user_id, $this->access_token());
 
         return $this->json_request($url);
@@ -231,7 +243,8 @@ class InstagramService extends RestfulService {
      * @param int user id
      * @return std_class other users that follow the one passed in
      */
-    public function userFollowedBy($user_id) {
+    public function userFollowedBy($user_id)
+    {
         $url = sprintf($this->api_urls['user_followed_by'], $user_id, $this->access_token());
 
         return $this->json_request($url);
@@ -242,7 +255,8 @@ class InstagramService extends RestfulService {
      * Accepts an access token
      * @return std_class users who have requested this user's permission to follow
      */
-    public function userRequestedBy() {
+    public function userRequestedBy()
+    {
         $url = sprintf($this->api_urls['user_requested_by'], $this->access_token());
 
         return $this->json_request($url);
@@ -253,7 +267,8 @@ class InstagramService extends RestfulService {
      * @param int user id
      * @return std_class user's relationship to another user
      */
-    public function userRelationship($user_id) {
+    public function userRelationship($user_id)
+    {
         $url = sprintf($this->api_urls['user_relationship'], $user_id, $this->access_token());
 
         return $this->json_request($url);
@@ -265,7 +280,8 @@ class InstagramService extends RestfulService {
      * @param string action to effect relatonship (follow/unfollow/block/unblock/approve/deny)
      * @return std_class result of request
      */
-    public function modifyUserRelationship($user_id, $action) {
+    public function modifyUserRelationship($user_id, $action)
+    {
         $url = sprintf($this->api_urls['modify_user_relationship'], $user_id, $action, $this->access_token());
 
         return $this->json_request($url);
@@ -277,7 +293,8 @@ class InstagramService extends RestfulService {
      * @param int media id
      * @return std_class data about the media item
      */
-    public function getMedia($media_id) {
+    public function getMedia($media_id)
+    {
         $url = sprintf($this->api_urls['media'], $media_id, $this->access_token());
 
         return $this->json_request($url);
@@ -293,7 +310,8 @@ class InstagramService extends RestfulService {
      * @param int distance
      * @return std_class media items found in search
      */
-    public function mediaSearch($latitude=null, $longitude=null, $max_timestamp=null, $min_timestamp=null, $distance=null) {
+    public function mediaSearch($latitude=null, $longitude=null, $max_timestamp=null, $min_timestamp=null, $distance=null)
+    {
         $url = sprintf($this->api_urls['media_search'], $latitude, $longitude, $max_timestamp, $min_timestamp, $distance);
 
         return $this->json_request($url);
@@ -303,7 +321,8 @@ class InstagramService extends RestfulService {
      * Function to get a list of what media is most popular at the moment
      * @return std_class popular media
      */
-    public function popularMedia() {
+    public function popularMedia()
+    {
         $url = sprintf($this->api_urls['media_popular'], $this->access_token());
 
         return $this->json_request($url);
@@ -314,7 +333,8 @@ class InstagramService extends RestfulService {
      * @param int media id
      * @return std_class media comments
      */
-    public function mediaComments($media_id) {
+    public function mediaComments($media_id)
+    {
         $url = sprintf($this->api_urls['media_comments'], $media_id, $this->access_token());
 
         return $this->json_request($url);
@@ -325,7 +345,8 @@ class InstagramService extends RestfulService {
      * @param int media id
      * @return std_class list of users
      */
-    public function mediaLikes($media_id) {
+    public function mediaLikes($media_id)
+    {
         $url = sprintf($this->api_urls['likes'], $media_id, $this->access_token());
 
         return $this->json_request($url);
@@ -336,7 +357,8 @@ class InstagramService extends RestfulService {
      * @param int media id
      * @return std_class response to request
      */
-    public function postLike($media_id) {
+    public function postLike($media_id)
+    {
         $url = sprintf($this->api_urls['post_like'], $media_id, $this->access_token());
 
         return $this->json_request($url, 'POST');
@@ -347,7 +369,8 @@ class InstagramService extends RestfulService {
      * @param int media id
      * @return std_class response to request
      */
-    public function removeLike($media_id) {
+    public function removeLike($media_id)
+    {
         $url = sprintf($this->api_urls['remove_like'], $media_id, $this->access_token());
 
         return $this->json_request($url);
@@ -358,7 +381,8 @@ class InstagramService extends RestfulService {
      * @param string tag
      * @return std_class of data about the tag
      */
-    public function getTags($tag) {
+    public function getTags($tag)
+    {
         $url = sprintf($this->api_urls['tags'], $tag, $this->access_token());
 
         return $this->json_request($url);
@@ -371,7 +395,8 @@ class InstagramService extends RestfulService {
      * @param int return media before this min_id
      * @return std_class recently tagged media
      */
-    public function tagsRecent($tag, $max_id=null, $min_id=null) {
+    public function tagsRecent($tag, $max_id=null, $min_id=null)
+    {
         $url = sprintf($this->api_urls['tags_recent'], $tag, $max_id, $min_id, $this->access_token());
 
         return $this->json_request($url);
@@ -382,7 +407,8 @@ class InstagramService extends RestfulService {
      * @param string valid tag name without a leading #. (eg. snow, nofilter)
      * @return std_class tags by name - results are ordered first as an exact match, then by popularity
      */
-    public function tagsSearch($tag) {
+    public function tagsSearch($tag)
+    {
         $url = sprintf($this->api_urls['tags_search'], $tag, $this->access_token());
 
         return $this->json_request($url);
@@ -393,7 +419,8 @@ class InstagramService extends RestfulService {
      * @param int location id
      * @return std_class data about the location
      */
-    public function getLocation($location) {
+    public function getLocation($location)
+    {
         $url = sprintf($this->api_urls['locations'], $location, $this->access_token());
 
         return $this->json_request($url);
@@ -408,7 +435,8 @@ class InstagramService extends RestfulService {
      * @param int return media before this UNIX timestamp
      * @return std_class recent media objects from a location
      */
-    public function locationRecent($location, $max_id=null, $min_id=null, $max_timestamp=null, $min_timestamp=null) {
+    public function locationRecent($location, $max_id=null, $min_id=null, $max_timestamp=null, $min_timestamp=null)
+    {
         $url = sprintf($this->api_urls['locations_recent'], $location, $max_id, $min_id, $max_timestamp, $min_timestamp, $this->access_token());
 
         return $this->json_request($url);
@@ -422,10 +450,10 @@ class InstagramService extends RestfulService {
      * @param int distance. Default is 1000m (distance=1000), max distance is 5000
      * @return std_class location data
      */
-    public function locationSearch($latitude=null, $longitude=null, $foursquare_id=null, $distance=null) {
+    public function locationSearch($latitude=null, $longitude=null, $foursquare_id=null, $distance=null)
+    {
         $url = sprintf($this->api_urls['locations_search'], $latitude, $longitude, $foursquare_id, $distance, $this->access_token());
 
         return $this->json_request($url);
     }
-
 }
